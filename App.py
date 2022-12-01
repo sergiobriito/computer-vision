fcd7df7e3da8599bcad5000fea9a894a8a1b44e8
Sérgio Brito <64617586+sergiobriito@users.noreply.github.com>
2022-12-01 14:40:21 -0300

    Update App.py

--- App.py
+++ App.py
@@ -5,8 +5,6 @@ import av
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
-    flipped = img[::-1,:,:]
-
     return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
